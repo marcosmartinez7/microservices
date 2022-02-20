1b2b794d8cfaba7e6e50b25e5dc7c39504063d66
Marcos Martínez <marcos.mmartinez@mercadolibre.com>
2022-02-20 20:02:15 -0300

    149

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,8 +1,22 @@
-import { Request, Response, NextFunction } from "express"
-export const errorHandler= (error: Error, req: Request, res: Response, next: NextFunction) =>{
-  console.log("Something went wrong", error);
-  res.status(400).send({
-    message: error.message
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (error instanceof CustomError) {
+    res.status(error.statusCode).send({
+      errors: error.serializeErrors(),
+    });
+  }
+  res.status(500).send({
+    errors: [
+      {
+        message: 'Something went wrong',
+      },
+    ],
   });
-  
-}
+};
