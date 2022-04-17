7819d7c6d24d4f7e1b5277bb376172aed99d793c
marcosmartinez7 <mmartinezciompi@gmail.com>
2022-04-17 19:42:55 -0300

    309

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from "crypto";
 import nats from "node-nats-streaming";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 // kubectl port-forward nats-depl-5f5fff85d5-cdzl6 4222:4222
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -8,24 +9,7 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true) // retry until ack
-    .setDeliverAllAvailable() // reply from first message
-    .setDurableName("service-name"); // store events with a mark if was processed or not, resume with the not proccessed (Ack). The idea is that with setDeliverAllAvailable we receive all the messages, but, we dont want to reprocess the already ACK ones.
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "queue-group-name",
-    options
-  );
-  subscription.on("message", (msg) => {
-    console.log(
-      "Received a message [" + msg.getSequence() + "] " + msg.getData()
-    );
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 stan.on("close", () => {
