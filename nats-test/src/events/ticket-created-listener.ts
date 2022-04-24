d31b58cdfd9c7226b12183ea657eac801460a57f
marcosmartinez7 <mmartinezciompi@gmail.com>
2022-04-24 00:25:09 -0300

    323

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-event";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payment-service";
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("Event data ", data);
     msg.ack();
   }
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,20 +1,22 @@
 import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 // kubectl port-forward nats-depl-5f5fff85d5-cdzl6 4222:4222
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published ", data);
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
